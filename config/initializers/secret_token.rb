741de588a18b76a369420940d35353a06da0ce13
Ryan Hirsch <Ryan.Hirsch@gmail.com>
2013-09-23 17:54:13 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Grades::Application.config.secret_key_base = '93f33e55060096259e58bf3a89a8ce9656eb4a8ebcfb8a3957e53d47cabc7e9f7136c51d100fe33e27e5ac9a2b717891382f29f0dc636057501496e081d57bb1'
